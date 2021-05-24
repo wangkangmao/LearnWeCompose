63f44445c6d72d4ccc06357a294aeae296502882
wangkm <1240413544@qq.com>
2021-05-24 08:39:26 +0800

    my WeCompose

---
+++ app/src/main/java/com/wangkmlearnwecompose/ui/Common.kt
@@ -0,0 +1,82 @@
+package com.wangkmlearnwecompose.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.wangkmlearnwecompose.ui.theme.WeTheme
+import com.wangkmlearnwecompose.R
+import dev.chrisbanes.accompanist.insets.navigationBarsPadding
+import dev.chrisbanes.accompanist.insets.statusBarsPadding
+
+/**
+ * @author: created by wangkm
+ * @time: 2021/05/23 22:35
+ * @desc：公共的主题
+ * @email: 1240413544@qq.com
+ */
+
+@Composable
+fun WeTopBar(title:String,onBack: (() -> Unit)? = null) {
+    Box (
+        Modifier
+            .background(WeTheme.colors.background)
+            .fillMaxWidth()
+            .statusBarsPadding()
+            ){
+        Row(
+            Modifier
+                .height(48.dp)
+        ) {
+            if(onBack !=null){
+                Icon(
+                   painterResource(R.drawable.ic_back),
+                    null,
+                    Modifier
+                        .clickable { onBack }
+                        .align(Alignment.CenterVertically)
+                        .size(36.dp)
+                        .padding(8.dp),
+                    tint = WeTheme.colors.icon
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            // todo WeViewModel
+//            val viewModel:WeV
+            Icon(painter = painterResource(id = R.drawable.ic_palette)
+                , contentDescription = "切换主题",
+                Modifier
+                    .clickable(onClick = {
+                        // todo choose theme
+                    })
+                    .align(Alignment.CenterVertically)
+                    .size(36.dp)
+                    .padding(8.dp),
+                tint = WeTheme.colors.icon
+            )
+        }
+        Text(title,Modifier.align(Alignment.Center),color = WeTheme.colors.textPrimary)
+    }
+
+}
+
+@Composable
+fun WeBottomBar(modifier: Modifier = Modifier, content: @Composable RowScope.() -> Unit) {
+
+    Row(
+        modifier
+            .fillMaxWidth()
+            .background(WeTheme.colors.bottomBar)
+            .padding(4.dp, 0.dp)
+            .navigationBarsPadding(),
+        content = content
+    )
+
+
+}
---
+++ app/src/main/java/com/wangkmlearnwecompose/ui/HomePage.kt
@@ -0,0 +1,16 @@
+package com.wangkmlearnwecompose.ui
+
+import androidx.compose.runtime.Composable
+
+/**
+ * @author: created by wangkm
+ * @time: 2021/05/23 22:34
+ * @desc：
+ * @email: 1240413544@qq.com
+ */
+
+@Composable
+fun HomeBottomBar(current:Int,currentChange:(Int) -> Unit){
+
+
+}
